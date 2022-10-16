8b234f814322962c4bf3079c803f26958dbf721d
tderuvo <tonyderuvo@mac.com>
2022-10-15 22:43:28 -0700

    added social icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function Header({}: Props) {
-  return <div>I am a Header</div>;
+  return (
+    <header>
+      <div>
+        {/* Social icons */}
+        <SocialIcon url="https://linkedin.com/in/jaketrent" />
+      </div>
+    </header>
+  );
 }
