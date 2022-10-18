8d46c1c2b2c69c324d11ee6b0a1cc8a268a368c0
tderuvo <tonyderuvo@mac.com>
2022-10-17 22:13:22 -0700

    added the circles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import BackgroundCircle from "./BackgroundCircle";
 
 type Props = {};
 
 export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
-    words: ["Tony's me name", "I love coffee not tea", "Creator"],
+    words: ["Tony's me name", "I love coffee not tea", "Creator", "Reddington"],
     loop: true,
     delaySpeed: 2000,
   });
   return (
-    <div>
-      <h1 className="text-center">
+    <div className="h-screen flex flex-col space-y-0 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircle />
+      <h1>
         <span>{text}</span>
         <Cursor cursorColor="#F7AB0A" />
       </h1>
