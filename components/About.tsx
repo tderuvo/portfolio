b9b2c911169111bba4cf7090529e3bedcf75506c
tderuvo <tonyderuvo@mac.com>
2022-10-30 21:20:30 -0700

    completed about section

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,12 @@ type Props = {};
 
 export default function About({}: Props) {
   return (
-    <div className="flex relative flex-col md:text-left h-screen md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex relative flex-col md:text-left h-screen md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className=" absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
@@ -23,6 +28,22 @@ export default function About({}: Props) {
         src="https://media-exp1.licdn.com/dms/image/C5603AQE86JM_9gwVGw/profile-displayphoto-shrink_800_800/0/1589904888343?e=1671667200&v=beta&t=Ninassu02F0QNncZsGquIZcNdiyCsqUkVaHZjWxnjoU"
         className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] "
       />
-    </div>
+      <div className=" space-y-10 px-0 md:px-10">
+        <h4 className=" text-4xl font-semibold">
+          Here is a{" "}
+          <span className=" underline decoration-[#F7AB0A]/30">little</span>{" "}
+          background
+        </h4>
+        <p className=" text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </motion.div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,7 @@ export default function Hero({}: Props) {
       />
       <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
-          Software Engineer
+          Ideator
         </h2>
         <h1 className=" text-5xl lg:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
